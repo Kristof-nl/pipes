6db930ce1e5f9ca24bae7d02b0183ec1896c28b7
Kristof-nl <hermaszewski.krzysztof@gmail.com>
2022-04-24 21:38:32 +0200

    Displaying JSON

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name!: string;
+  date!: string;
+  amount!: number;
+  height!: number;
+
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000
+  }
+
+  onHeightChange(value: string)
+  {
+    this.height = parseFloat(value);
+  }
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
 }
